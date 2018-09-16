76917534a36a0e348a1f70bc933a8ad9427cda4b
Frank Pizzuta <frank@pizzuta.com>
2018-09-16 12:13:57 -0400

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<?php require('p1.php'); ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8" />
+    <title>Project 1</title>
+    <link href="p1.css" rel="stylesheet">
+</head>
+
+<body>
+	<h2>Quote of the day</h2>
+	<div>
+		<span id="quote"> <?php echo $quote; ?> </span>
+	</div>
+</body>
+</html>
---
+++ p1.php
@@ -0,0 +1,4 @@
+<?php
+
+$quotes = ["I'll be back","Hasta la vista, baby", "Get to the chopper!","It's not a tumor"];
+$quote = $quotes[array_rand($quotes)];
