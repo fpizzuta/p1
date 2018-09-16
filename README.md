c20b54f7a2a98755274f70b21d813d6395ccec86
Frank Pizzuta <frank@pizzuta.com>
2018-09-16 12:46:56 -0400

    html and php added

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-"# p1" 
+# Project 1
++ By: Frank Pizzuta
++ Production URL: <http://p1.yourdomain.com>
+
+## Outside resources
+[https://stackoverflow.com/questions/4233407/get-random-item-from-array]
+[https://stackoverflow.com/questions/31217268/center-div-on-the-middle-of-screen]
+
+## Notes for instructor
+
+
--- index.php
+++ index.php
@@ -8,9 +8,16 @@
 </head>
 
 <body>
-	<h2>Quote of the day</h2>
-	<div>
-		<span id="quote"> <?php echo $quote; ?> </span>
+	<div id="wrapper">
+		<h1>Frank Pizzuta</h1>
+		<div id="imageFrame">
+			<img id="pic" src="images/me.jpg" alt="Picture of me">
+		</div>
+		<p>My daughter thought it was funny to make me into a white walker for the photo. I have worked at a financial services company in Oaks, PA for the last 25 years. I love learning new things. Harvard will be my 6th degree!</p>
+		<h2>Quote of the day</h2>
+		<div>
+			<span id="quote"> <?php echo $quote; ?> </span>
+		</div>
 	</div>
 </body>
 </html>
